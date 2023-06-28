ea21af24a250473a7d0688a13ae1cab61dd8cc07
Chris Newland <chris@chrisnewland.com>
2023-06-28 23:02:57 +0100

    LNT and access flags minor improvements

--- src/main/java/com/chrisnewland/classact/model/attribute/LineNumberTable.java
+++ src/main/java/com/chrisnewland/classact/model/attribute/LineNumberTable.java
@@ -4,17 +4,30 @@ import com.chrisnewland.classact.model.AttributeType;
 import com.chrisnewland.classact.model.IntegerIntegerMap;
 import com.chrisnewland.classact.model.constantpool.ConstantPool;
 
+import java.util.Map;
+
 public class LineNumberTable extends IntegerIntegerMap implements Attribute
 {
-	@Override
-	public AttributeType getType()
-	{
-		return AttributeType.LineNumberTable;
-	}
+    @Override
+    public AttributeType getType()
+    {
+        return AttributeType.LineNumberTable;
+    }
 
-	@Override
-	public String toString(ConstantPool constantPool)
-	{
-		return toString();
-	}
+    @Override
+    public String toString(ConstantPool constantPool)
+    {
+        StringBuilder builder = new StringBuilder();
+
+        for (Map.Entry<Integer, Integer> entry : entrySet())
+        {
+            builder.append("bci: ")
+                    .append(entry.getKey())
+                    .append(" line: ")
+                    .append(entry.getValue())
+                    .append("\n");
+        }
+
+        return builder.toString();
+    }
 }
